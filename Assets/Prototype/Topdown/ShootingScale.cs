e3197bcbc4d462fe32b2ca6598cc15ba70cc7ade
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-11-13 13:02:51 +0800

    get closest bullet

---
+++ Assets/Prototype/Topdown/ShootingScale.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype.Topdown{
+	public class ShootingScale : MonoBehaviour{
+		[SerializeField] private List<GameObject> scales;
+		[SerializeField] private GameObject target;
+
+
+		private GameObject GetCloseScale(){
+			float minDot = 0;
+			GameObject closeScale = null;
+			foreach(var scale in scales){
+				var dot = Vector3.Dot(scale.transform.up, target.transform.position - scale.transform.position);
+				if(dot < minDot) continue;
+				closeScale = scale;
+				minDot = dot;
+			}
+			return closeScale;
+		}
+
+		private void OnDrawGizmos(){
+			if(!target) return;
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(transform.position, target.transform.position);
+			Gizmos.color = Color.red;
+			var closeScale = GetCloseScale();
+			Gizmos.DrawLine(closeScale.transform.position , target.transform.position);
+		}
+	}
+}
